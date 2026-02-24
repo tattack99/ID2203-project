b24504fd61b29f4dad3daeb17363b13327f8a5b9
Your Name <you@example.com>
2026-02-24 16:51:15 +0100

    added the test delay code as comments

--- src/shim/main.rs
+++ src/shim/main.rs
@@ -2,7 +2,7 @@ use shim::Shim;
 use configs::ShimConfig;
 use core::panic;
 use env_logger;
-
+// use std::{thread, time::Duration};
 mod configs;
 mod shim;
 mod network;
@@ -15,6 +15,10 @@ pub async fn main(){
         Ok(parsed_config) => parsed_config,
         Err(e) => panic!("{e}"),
     };
+    // let t = true;
+    // while t{
+    //     Duration::from_secs(2);
+    // }
     let mut shim = Shim::new(shim_config).await;
     shim.run().await;
 }
