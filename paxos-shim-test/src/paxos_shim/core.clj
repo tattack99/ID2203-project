590338ea1b96ce9bdf9321cb4e667657ed43d62c
Tim Johansson <timmon.johansson@gmail.com>
2026-03-01 19:54:27 +0100

    50/50 read writes

--- paxos-shim-test/src/paxos_shim/core.clj
+++ paxos-shim-test/src/paxos_shim/core.clj
@@ -23,16 +23,23 @@
           :os        os/noop
           :db        db/noop
           :remote    noop-remote
+          :concurrency 10 ; 10 parallel workers
           :client    (paxos-client/paxos-client)
           :checker   (checker/compose
                       {:linear (checker/linearizable {:model (model/register)
                                                       :algorithm :linearizable})})
-          :generator (->> (gen/mix [{:f :read} 
-                                    (fn [test process] {:f :write, :value (rand-int 100)})])
-                          (gen/stagger 1/10)
-                          (gen/limit 20)
-                          (gen/clients))}
+          :generator (->> (fn [test process]
+                            ;; This function is called for every single operation
+                            ;; 50% chance for :read, 50% chance for :write
+                            (if (< (rand) 0.5)
+                              {:f :read}
+                              {:f :write :value (rand-int 100)}))
+                          (gen/stagger 1/5)    ; 200ms delay between ops per thread
+                          (gen/time-limit 60)  ; Run for 60 seconds
+                          (gen/clients))
+}      ; Distribute among the 10 workers
          opts))
+
 
 (defn -main [& args]
   (println "🛡️ Starting Jepsen Test...")
